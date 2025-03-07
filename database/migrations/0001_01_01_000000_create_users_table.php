414bf2ac077dc6ee4d8ad64e652be4c96b61dada
nickznjk <nickznjk@gmail.com>
2025-03-07 17:20:38 +0800

    ユーザーテーブルの作成

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,16 +13,15 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('user_id', 50)->unique()->comment("ユーザーID");
+            $table->string('name', 50)->comment("名前");
+            $table->string('password')->comment("パスワード");
             $table->rememberToken();
             $table->timestamps();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('user_id')->primary();
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
