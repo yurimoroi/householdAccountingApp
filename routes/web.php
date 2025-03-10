f662b6e97601e9b81ff132a93ab713889de4ca8b
face2-admin <yuri.4545football@gmail.com>
2025-03-10 13:41:28 +0800

    ルーティングを追加

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,17 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Controller;
 
+
+
+Route::get('/login',  [Controller::class, 'getLogin']);
+Route::post('/login', [Controller::class, 'login'])->name('login');
+Route::post('/logout', [Controller::class, 'logout'])->name('logout');
+
+Route::redirect('/', '/dashboard', 302);
+Route::get("/dashboard", [Controller::class, "getDashboard"]);
+Route::get("/report", [Controller::class, "getReport"]);
+
+Route::post("/upsert", [RestController::class, "upsertItems"])->middleware("auth");
+Route::post("/delete", [RestController::class, "deleteItems"])->middleware("auth");
