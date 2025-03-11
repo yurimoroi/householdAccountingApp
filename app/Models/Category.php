87c05cf178135d456e9e0ed8877c76f618aad505
face2-admin <yuri.4545football@gmail.com>
2025-03-11 15:17:24 +0800

    カテゴリーモデルの名前修正・fillableを追加

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,12 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class categories extends Model
+class Category extends Model
 {
-    //
+
+    protected $table = "categories";
+
     public $timestamps = false;
 
     protected $fillable = [
+        'type',
         'user_id',
         'name'
     ];
